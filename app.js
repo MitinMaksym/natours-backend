032aad910389e93582e5e88a271c81068cb02492
Maks Mitin <mitinmaksym@gmail.com>
2021-05-02 19:07:26 +0300

    feat: conect to mongodb

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const tourRoute = require('./routes/toursRoutes');
 const usersRoute = require('./routes/usersRoutes');
 
 const app = express();
-
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 /// ROUTES
 app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', usersRoute);
 
 module.exports = app;
+// npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
--- server.js
+++ server.js
@@ -1,6 +1,48 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = 3000;
+dotenv.config({ path: `${__dirname}/config.env` });
+const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB is connected');
+  })
+  .catch((err) => console.log(err));
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name should be provided'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
+
+const newTour = new Tour({ name: 'Sea Observer', price: 333 });
+newTour
+  .save()
+  .then((data) => console.log(data))
+  .catch((e) => console.log(e));
 
 app.listen(port, '127.0.0.1', () => {
   console.log('App running on port ' + port);
