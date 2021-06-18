fa1d04ffe7af8f05b1ddd8a742ed9aafae390504
Mitin Maks <mitinmaksym@gmail.com>
2021-06-18 17:46:49 +0300

    feat: send token with cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,16 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
-
+    const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    };
+      if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    
+      res.cookie('jwt', token, cookieOptions);
+    user.password = undefined;
     res.status(statusCode).send({
         status: 'success',
         token,
