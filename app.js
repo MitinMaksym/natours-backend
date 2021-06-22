d6f14eee7f6ad315dd152f6bc537814980402fdb
Mitin Maks <mitinmaksym@gmail.com>
2021-06-22 10:26:16 +0300

    feat: setting security headers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 const morgan = require('morgan');
 const AppError = require('./utils/AppError');
 const errorController = require('./controllers/errorController');
@@ -8,17 +9,28 @@ const tourRoute = require('./routes/toursRoutes');
 const usersRoute = require('./routes/usersRoutes');
 
 const app = express();
+//GLOBAL MIDDLEWARES
+
+//Security http headers
+app.use(helmet())
+
+//Development loging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Limit requests from same API
 const limiter = rateLimit({
   max: 300,
   windowMs:60 * 60 * 1000,
   message: 'Too many requests. Please try again in an hour'
 })
-app.use(limiter)
+app.use('/app',limiter)
+
+//Body parser. Reading data from body into req.body
 app.use(express.json());
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 /// ROUTES
