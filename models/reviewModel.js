7666a7efd3b046006b51edd7ad8d039c4c92f561
Mitin Maks <mitinmaksym@gmail.com>
2021-06-29 17:49:48 +0300

    feat: populate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -29,6 +29,18 @@ const reviewSchema = new mongoose.Schema(
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'user',
+    select: 'name',
+  }).populate({
+    path: 'tour',
+    select: 'name',
+  });
+
+  next()
+})
+
 const reviewModel = mongoose.model('Review', reviewSchema)
 
 module.exports = reviewModel
---
+++ routes/usersRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const usersController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router
+  .route('/signup')
+  .post(authController.signup);
+
+  router
+  .route('/login')
+  .post(authController.login);
+
+  router
+  .route('/forgotPassword')
+  .post(authController.forgotPassword);
+  
+  router
+  .route('/resetPassword/:token')
+  .patch(authController.resetPassword);
+
+  router
+  .route('/updatePassword')
+  .patch(authController.protect, authController.updatePassword);
+
+  router
+  .route('/updateMe')
+  .patch(authController.protect, usersController.updateUserById);
+
+  router
+  .route('/deleteMe')
+  .delete(authController.protect, usersController.deleteMe);
+
+  router
+  .route('/')
+  .get(usersController.getUsers)
+  .post(usersController.createUser);
+
+  router
+  .route('/:id')
+  .get(usersController.getUserById)
+  // .patch(usersController.updateUserById)
+  .delete(usersController.deleteUserById);
+
+module.exports = router;
