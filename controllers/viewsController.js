9d505b94a63111dcc5b968b92b17ac59a732b931
MitinMaksym <mitinmaksym@gmail.com>
2021-08-01 15:42:35 +0300

    feat: create tour overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,10 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find({});
+  res.status(200).render('overview', { tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The forest hiker' });
